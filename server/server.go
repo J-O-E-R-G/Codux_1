a61c4eead274baf64e45a48c41010af711e0dae0
Codux <codux@codux.com>
2023-07-07 18:20:48 -0500

    testing server refactor

--- server/server.go
+++ server/server.go
@@ -5,13 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Game struct {
-	Category  string `json:"category"`
-	Message   string `json:"message"`
-	GameName  string `json:"GameName"`
-	GameAbout string `json:"GameAbout"`
-}
-
 func main() {
 	r := gin.Default()
 
@@ -24,34 +17,7 @@ func main() {
 		AllowCredentials: true,
 	}))
 
-	r.GET("/games/:category", func(c *gin.Context) {
-		category := c.Param("category")
-
-		var game Game
-
-		if category == "biology" {
-			game = Game{
-				Category:  "biology",
-				Message:   "Welcome to biology games",
-				GameName:  "Guess the Animal",
-				GameAbout: "Read Characteristics of a given species and give your best guess",
-			}
-		} else if category == "history" {
-			game = Game{
-				Category:  "history",
-				Message:   "Welcome to history games",
-				GameName:  "Guess the Guy",
-				GameAbout: "Read Characteristics and Achievments and give your best guess",
-			}
-		} else {
-			c.JSON(404, gin.H{
-				"status": "not found",
-			})
-			return
-		}
-
-		c.JSON(200, game)
-	})
+	r.GET("/games/:category", get_route)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
